1dcdacc94511213722f7547a46efa1e346dc1966
hawk198723 <hawk198723@gmail.com>
2023-01-14 15:46:12 -0800

    Init commit

---
+++ src/main/java/com/StudentTrack/StudentTrackApplication.java
@@ -0,0 +1,13 @@
+package com.StudentTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StudentTrack/StudentTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
