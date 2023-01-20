2df5135840d20ce509d3469d575309547daf4c5b
hawk198723 <hawk198723@gmail.com>
2023-01-19 23:52:07 -0800

    DI and connect DB

--- src/main/java/com/StudentTrack/controller/StudentController.java
+++ src/main/java/com/StudentTrack/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.StudentTrack.controller;
 
 import com.StudentTrack.model.Student;
 import com.StudentTrack.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +14,14 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
     @GetMapping
     public Student getStudents(){
-
         return studentService.getStudents();
-
     }
 
 }
